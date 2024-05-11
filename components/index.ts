9485f4d0b621254a4629b8a37697cb6570b74d91
Jiayi Jiang <jiayijiangwork@outlook.com>
2024-05-12 02:05:43 +1000

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import SearchManufacture from './SearchManufacture'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
+import ShowMore from './ShowMore'
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacture,
+    CarCard,
+    CarDetails,
+    ShowMore
+}
